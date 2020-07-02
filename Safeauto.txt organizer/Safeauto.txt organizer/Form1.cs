c70aaa0dd885fe9626176f3905cc5f599fc7b4f7
Andres Morales <43222504+Testing42@users.noreply.github.com>
2020-07-01 23:17:38 -0400

    Add files via upload

---
+++ Safeauto.txt organizer/Safeauto.txt organizer/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Safeauto.txt_organizer
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		OpenFileDialog ofd = new OpenFileDialog();
+
+		string text = "";
+
+		private void TxtButton_Click(object sender, EventArgs e)
+		{
+			ofd.Filter = "TXT|*.txt";
+			if (ofd.ShowDialog() == DialogResult.OK)
+			{
+				fileName.Text = ofd.SafeFileName;
+				fileLocation.Text = ofd.FileName;
+				StreamReader sr = new StreamReader(ofd.FileName);
+				while ((text = sr.ReadLine()) != null)
+				{
+
+
+					if (text != null)
+					{
+
+						listBox1.Items.Add(text);
+
+					}
+				}
+				sr.Close();
+			}
+		}
+
+
+
+		private void Reset_Click(object sender, EventArgs e)
+		{
+			fileName.Text = String.Empty;
+			fileLocation.Text = String.Empty;
+			listBox1.Items.Clear();
+		}
+
+
+	}
+}
